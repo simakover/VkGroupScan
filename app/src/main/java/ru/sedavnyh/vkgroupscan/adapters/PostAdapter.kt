3360ecbe08e6233e5aa1a57b073f5dfa608ff333
Simakover <Simakover@gmail.com>
2021-08-13 14:57:29 +0500

    added icon

--- app/src/main/java/ru/sedavnyh/vkgroupscan/adapters/PostAdapter.kt
+++ app/src/main/java/ru/sedavnyh/vkgroupscan/adapters/PostAdapter.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
