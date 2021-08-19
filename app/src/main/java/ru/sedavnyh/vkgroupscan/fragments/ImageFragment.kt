e8ff659f05e11ddc251d01f9571c2bad067d1dd7
Simakover <Simakover@gmail.com>
2021-08-20 03:02:35 +0500

    small changes to style

--- app/src/main/java/ru/sedavnyh/vkgroupscan/fragments/ImageFragment.kt
+++ app/src/main/java/ru/sedavnyh/vkgroupscan/fragments/ImageFragment.kt
@@ -10,6 +10,8 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import coil.load
+import com.google.android.material.tabs.TabLayout
+import ru.sedavnyh.vkgroupscan.R
 import ru.sedavnyh.vkgroupscan.databinding.FragmentImageBinding
 
 class ImageFragment : Fragment() {
@@ -19,6 +21,7 @@ class ImageFragment : Fragment() {
     private var linkImage: String = ""
     private lateinit var activity : AppCompatActivity
     private var uiHiden = false
+    lateinit var tabLayout : TabLayout
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreateView(
@@ -27,6 +30,8 @@ class ImageFragment : Fragment() {
     ): View? {
         _binding = FragmentImageBinding.inflate(inflater, container, false)
         binding.fullImageImageView.load(linkImage)
+
+        tabLayout = requireActivity().findViewById(R.id.tabLayout)!!
 
         activity  = requireActivity() as AppCompatActivity
         activity.supportActionBar?.hide()
@@ -48,10 +53,12 @@ class ImageFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.R)
     private fun hideSystemUI() {
         uiHiden = true
+        tabLayout.visibility = View.GONE
         activity.window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
     private fun showSystemUI() {
         uiHiden = false
+        tabLayout.visibility = View.VISIBLE
         activity.window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 }
